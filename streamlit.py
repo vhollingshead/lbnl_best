0c7b1e8722bb5621500987b3b7d792b1a56d273d
Victoria Hollingshead <vhollingshead@ischool.berkeley.edu>
2025-07-13 17:22:29 -0700

    update button

--- streamlit.py
+++ streamlit.py
@@ -366,7 +366,7 @@ def home():
                 </div>
             """, unsafe_allow_html=True)
 
-            col1, col2, col3 = st.columns([1, 2, 1])
+            col1, col2, col3 = st.columns([1, 3, 1])
             with col2:
                 if st.button("Get Started"):
                     st.session_state.page = "Get Started"
