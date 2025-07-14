220cf9c3252565cf5621e986706000855f0655a7
Victoria Hollingshead <vhollingshead@ischool.berkeley.edu>
2025-07-14 15:12:12 -0700

    landign page

--- streamlit.py
+++ streamlit.py
@@ -332,6 +332,19 @@ import base64
 from datetime import date
 st.set_page_config(layout="wide")
 
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # Load and encode image
 with open("best_all_green_xhigh.png", "rb") as img_file:
     encoded_best_high = base64.b64encode(img_file.read()).decode()
